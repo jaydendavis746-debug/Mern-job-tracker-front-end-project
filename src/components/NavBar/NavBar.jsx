4bca78fa1a0b99bf2fb21838fb6d49f98002b469
Jayden Davis <jaydendavis746@gmail.com>
2026-02-10 04:39:01 +0000

    Conditional rendering skipped userServices

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,10 +16,13 @@ const NavBar = () => {
             {user ? (
                 <ul>
                     <li>Welcome, {user.username}</li>
+                    <li><Link to='/'>Dashboard (Logo/name)</Link></li>
                      <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+                    
                 </ul>
             ) : (
                 <ul>
+                    <li><Link to='/'>Home (Logo/name)</Link></li>
                     <li><Link to='/sign-up'>Sign Up</Link></li>
                      <li><Link to='/sign-in'>Sign In</Link></li>
                     
